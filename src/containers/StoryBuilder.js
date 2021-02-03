f3f2d3fcd7222c70d4093db9ccd15ca07c7a9537
mpottebaum <mpottebaum@gmail.com>
2021-02-02 23:33:24 -0600

    added axios and babel runtime

--- src/containers/StoryBuilder.js
+++ src/containers/StoryBuilder.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Grid from '../components/Grid'
 
 const StoryBuilder = () => {
+
+    const [ story, setStory ] = useState(null)
+
+    console.log('story', story)
+
+    useEffect(() => {
+        getStory()
+    }, [])
+
+    const getStory = async () => {
+        axios('http://localhost:3000/stories/1')
+            .then(storyResp => setStory(storyResp.data))
+    }
+
     return <div>
         <Grid />
     </div>
