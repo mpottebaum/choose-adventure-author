f912de84a676e74a2126d3a1b5c812802600040b
mpottebaum <mpottebaum@gmail.com>
2021-02-18 15:47:09 -0600

    added react-modal

--- src/containers/StoryBuilder.js
+++ src/containers/StoryBuilder.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
 import Toolbar from '../components/Toolbar'
 import Grid from '../components/Grid'
+import ReactModal from 'react-modal'
 
 const Container = styled.div`
     height: 50vh;
@@ -16,6 +17,7 @@ const StoryBuilder = () => {
         x: 0,
         y: 0
     })
+    const [ storyNodeModal, setStoryNodeModal ] = useState(false)
 
     useEffect(() => {
         getStory()
@@ -40,6 +42,14 @@ const StoryBuilder = () => {
             yCells={15}
         />
         <Toolbar />
+        <ReactModal
+            isOpen={storyNodeModal}
+            shouldCloseOnOverlayClick={true}
+            onRequestClose={() => setStoryNodeModal(false)}
+            ariaHideApp={false}
+        >
+            <p>story node modal</p>
+        </ReactModal>
     </Container>
 }
 
