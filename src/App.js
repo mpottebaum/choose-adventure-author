29fe042fb5e6eef5119d58264a0fb69e08a05bc9
mpottebaum <mpottebaum@gmail.com>
2021-02-18 17:33:22 -0600

    added redux + setup empty store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import StoryBuilder from './containers/StoryBuilder'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/index'
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const App = () => {
 
-    return <StoryBuilder />
+    return (
+        <Provider store={store}>
+            <StoryBuilder />
+        </Provider>
+    )
 }
 
 export default App
