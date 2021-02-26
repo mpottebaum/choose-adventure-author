9bba93600cb845b91d283b6cddce65aaba2bf3a7
mpottebaum <mpottebaum@gmail.com>
2021-02-26 14:35:41 -0600

    added change gridViewCenter actions to grid nav arrow btns

--- src/store/gridViewCenter/actions.js
+++ src/store/gridViewCenter/actions.js
@@ -1,6 +1,20 @@
-import { SET_GRID_VIEW_CENTER } from './index'
+import {
+    SET_GRID_VIEW_CENTER,
+    SET_GRID_VIEW_X,
+    SET_GRID_VIEW_Y
+} from './index'
 
 export const setGridViewCenter = gridViewCenter => ({
     type: SET_GRID_VIEW_CENTER,
     gridViewCenter
+})
+
+export const setGridViewX = x => ({
+    type: SET_GRID_VIEW_X,
+    x,
+})
+
+export const setGridViewY = y => ({
+    type: SET_GRID_VIEW_Y,
+    y
 })
--- src/store/gridViewCenter/index.js
+++ src/store/gridViewCenter/index.js
@@ -1,4 +1,6 @@
 export const SET_GRID_VIEW_CENTER = 'SET_GRID_VIEW_CENTER'
+export const SET_GRID_VIEW_X = 'SET_GRID_VIEW_X'
+export const SET_GRID_VIEW_Y = 'SET_GRID_VIEW_Y'
 
 const initialGridViewCenter = {
     x: 0,
@@ -9,6 +11,16 @@ const gridViewCenterReducer = ( state = initialGridViewCenter, action ) => {
     switch(action.type) {
         case SET_GRID_VIEW_CENTER:
             return action.gridViewCenter
+        case SET_GRID_VIEW_X:
+            return {
+                y: state.y,
+                x: action.x,
+            }
+        case SET_GRID_VIEW_Y:
+            return {
+                x: state.x,
+                y: action.y,
+            }
         default:
             return state
     }
